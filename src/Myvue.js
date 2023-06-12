99b0543a1f540d006f9affa30a3b7eb03755016c
hhvcg <719713496@qq.com>
2023-06-12 23:37:27 +0800

    add:数据代理

--- src/Myvue.js
+++ src/Myvue.js
@@ -1,7 +1,22 @@
-class Myvue {
+class MyVue {
     constructor(vm) {
         this.vm = vm
         this.$data = vm.data
         this.$el = vm.el
+        this.proxyData()
+    }
+    proxyData() {
+        for (let key in this.$data) {
+            Object.defineProperty(this, key, {
+                configurable: false, 
+                enumerable: true,
+                get() {
+                    return this.$data[key]
+                },
+                set(newVal) {
+                    this.$data[key] = newVal
+                }
+            })
+        }
     }
 }
